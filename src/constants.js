834839f65ccabd580cbdd2388eb4a4d4b03a55a1
Gabriel Rocha <gabrielrs7@outlook.com>
2022-06-08 22:47:04 -0300

    refactor: changing number of rows

--- src/constants.js
+++ src/constants.js
@@ -3,13 +3,17 @@ const canvas = document.getElementById('gameBoard')
 const ctx = canvas.getContext('2d')
 const startElement = document.getElementById('start')
 
+const boxElement = document.getElementById('box')
+
 const BOARD = []
 
 const rectSize = 20
 const numberOfSquaresX = 12
-const numberOfSquaresY = 20
-const boardHeight = rectSize*numberOfSquaresY
+const numberOfSquaresY = 21
+const boardHeight = rectSize*(numberOfSquaresY)
 const boardWidth = rectSize*numberOfSquaresX
+
+boxElement.style.height = rectSize*(numberOfSquaresY-1)+'px'
 
 canvas.height = boardHeight
 canvas.width = boardWidth
